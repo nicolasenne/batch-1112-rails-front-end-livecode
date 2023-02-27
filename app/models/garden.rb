e0c0e68d47f5925379b195ae0fc7b90acb898c18
Nicolas Enne <40183143+nicolasenne@users.noreply.github.com>
2023-02-27 18:23:00 -0300

    update

---
+++ app/models/garden.rb
@@ -0,0 +1,7 @@
+class Garden < ApplicationRecord
+  has_many :plants, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+  validates :banner_url, presence: true
+  has_many :plants, dependent: :destroy
+end
